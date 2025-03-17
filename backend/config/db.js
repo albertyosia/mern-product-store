35bff09f5543f956593baa91360c92fa41f93851
Albert Yosia <albertyosia77@gmail.com>
2025-03-17 21:15:10 +0800

    Add CRUD API

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,10 @@
-import mongoose from 'mongoose'
+import mongoose from 'mongoose';
 export const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI)
-    console.log(`MongoDB connected: ${conn.connection.host}`)
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
-    console.error(`Error: ${error.message}`)
-    process.exit(1)
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
   }
-}
+};
--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,19 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import { connectDB } from './config/db.js'
+import express from 'express';
+import dotenv from 'dotenv';
+import { connectDB } from './config/db.js';
 
-dotenv.config()
-const app = express()
+import productRoute from './routes/product.route.js';
 
-app.get('/', (req, res) => {
-  res.send('Server is ready')
-})
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+
+app.use('/api/products', productRoute);
 
 app.listen(5000, () => {
-  connectDB()
-  console.log('Server started at http://localhost:5000')
-})
+  connectDB();
+  console.log('Server started at http://localhost:5000');
+});
 
 //
