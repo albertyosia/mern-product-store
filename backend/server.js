9f7bff11a8cbd3421197e6c25d55d94d84468cf6
Albert Yosia <albertyosia77@gmail.com>
2025-03-18 20:39:16 +0800

    fix validation when deleting product

--- backend/server.js
+++ backend/server.js
@@ -7,13 +7,15 @@ import productRoute from './routes/product.route.js';
 dotenv.config();
 const app = express();
 
+const PORT = process.env.PORT || 3001;
+
 app.use(express.json());
 
 app.use('/api/products', productRoute);
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
   connectDB();
-  console.log('Server started at http://localhost:5000');
+  console.log('Server started at http://localhost:' + PORT);
 });
 
 //
